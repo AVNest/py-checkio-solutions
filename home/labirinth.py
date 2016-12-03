25514172dd37efb75fdd8e9a276a93b4f7680238
vefremov <efremov.vitaly@gmail.com>
2016-12-03 22:08:44 +0700

    Added labirinth

--- home/labirinth.py
+++ home/labirinth.py
@@ -1,6 +1,6 @@
 directions = ((0, 1, 'S'), (1, 0, 'E'), (0, -1, 'N'), (-1, 0, 'W'))
 done = False
-def get_exit_route(lab, position=(1, 1), path=[], route=[]):
+def get_exit_route(lab, position=(1, 1), path=[], route=''):
     global done
     if done:
         return
@@ -18,9 +18,8 @@ def get_exit_route(lab, position=(1, 1), path=[], route=[]):
 
         if 0 <= y < 12 and 0 <= x < 12 and (x, y) not in path and not lab[x][y]:
             path.append((x, y))
-            route.append(direction)
-            get_exit_route(lab, position=(x, y))
-    route.pop()
+            route += direction
+            get_exit_route(lab, position=(x, y), route=route)
 
 
 get_exit_route([
