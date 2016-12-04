7e42aa420fec62fcd377d853a166d3ec69cae46d
vefremov <efremov.vitaly@gmail.com>
2016-12-04 16:00:45 +0700

    Improving labirinth

--- home/labirinth.py
+++ home/labirinth.py
@@ -1,28 +1,50 @@
-directions = ((0, 1, 'S'), (1, 0, 'E'), (0, -1, 'N'), (-1, 0, 'W'))
-done = False
-def get_exit_route(lab, position=(1, 1), path=[], route=''):
-    global done
-    if done:
-        return
+directions = ((1, 0, 'S'), (0, 1, 'E'), (0, -1, 'W'), (-1, 0, 'N'))
+final_route = None
+
+def get_exit_route(lab, position=(1, 1), path=((1, 1)), route=''):
+    global final_route
+    if final_route:
+        return final_route
 
     if position == (10, 10):
-        print('Yeah')
-        done = True
-        print(route)
-        return
-    print(position)
+        final_route = route
+        return route
+
     for x_inc, y_inc, direction in directions:
         x, y = position
         x += x_inc
         y += y_inc
 
         if 0 <= y < 12 and 0 <= x < 12 and (x, y) not in path and not lab[x][y]:
-            path.append((x, y))
+            path += ((x, y), )
             route += direction
-            get_exit_route(lab, position=(x, y), route=route)
+            get_exit_route(lab, (x, y), path, route)
+
+    return final_route
+
+def get_exit_route1(lab):
+    def visit_point(i, j, direction):
+        if 0 <= i < 12 and 0 <= j < 12 and (i, j) not in path and not lab[i][j]:
+            stack.append((i, j, route+direction))
+
+    end = (10, 10)
+    route = ''
+    path = []
+    stack = [(1, 1, '')]
+    while stack:
+        r, c, route = stack.pop()
+        if (r, c) == end:
+            print(route)
+            return route
+
+        path.append((r, c))
+        visit_point(r-1, c, 'N')
+        visit_point(r, c-1, 'W')
+        visit_point(r, c+1, 'E')
+        visit_point(r+1, c, 'S')
 
 
-get_exit_route([
+assert get_exit_route1([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
@@ -34,6 +56,6 @@ get_exit_route([
     [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]) == 'SSSSSSSSSEENENNEESEEEESS'
 
 print('Good')
