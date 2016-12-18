08f3184a6f70b539f47edd85651e4524c35fafba
vefremov <efremov.vitaly@gmail.com>
2016-12-18 21:57:53 +0700

    Added striped words

--- oreilly/striped_words.py
+++ oreilly/striped_words.py
@@ -1,10 +1,14 @@
 import string
+import re
 
 def checkio(text):
     vovels = 'aeiouy'
-    consonants = set(string.ascii_lowercase) - set(vovels)
-
-
+    key = lambda x: 0 if x in vovels else 1
+    return sum(
+        all(key(x) != key(y) for x, y in zip(word[::2], word[1::2]))
+        for word in re.findall(r'\w+', text) if len(word) > 1
+    )
+        
 assert checkio("My name is ...") == 3
 assert checkio("Hello world") == 0
 assert checkio("A quantity of striped words.") == 1
