b67f32d05a642bcd2965be0fed62461eebfb5b14
vefremov <efremov.vitaly@gmail.com>
2017-01-09 18:55:24 +0700

    Added xo game

--- oreilly/median.py
+++ oreilly/median.py
@@ -1,4 +1,8 @@
+"""
+Module
+"""
 def median(array):
+    """ Median function """
     n = len(array)
     array = sorted(array)
     mid = n // 2
@@ -7,15 +11,18 @@ def median(array):
     else:
         return array[mid]
 
+
 def checkio(data):
     data.sort()
     half = len(data) // 2
     return (data[half] + data[~half]) / 2
 
-assert median([]) == 0 
+assert median([]) == 0
 assert median([1, 2, 3, 4, 5]) == 3
 assert median([3, 1, 2, 5, 3]) == 3
 assert median([1, 300, 2, 200, 1]) == 2
 assert median([3, 6, 20, 99, 10, 15]) == 12.5
 
 print('Good')
+
+
--- oreilly/xo_game.py
+++ oreilly/xo_game.py
@@ -1,5 +1,15 @@
-def checkio(field):
-    print(field)
+def checkio(data):
+    n = len(data)
+    rows = data
+    cols = list(zip(*data))
+    diagonal1 = [r[i] for i, r in enumerate(data)]
+    diagonal2 = [r[i] for i, r in zip(range(n-1, -1, -1), data)]
+    testing_variants = rows + cols + [diagonal1, diagonal2]
+    for player in 'XO':
+        if any((all(e == player for e in variant) for variant in testing_variants)):
+            return player
+
+    return 'D'
 
     
 assert checkio([
@@ -10,6 +20,10 @@ assert checkio([
     "OO.",
     "XOX",
     "XOX"]) == "O"
+assert checkio([
+    "OO.",
+    "XOX",
+    "XXO"]) == "O"
 assert checkio([
     "OOX",
     "XXO",
