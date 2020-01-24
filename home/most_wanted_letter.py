bbac0bfb9ed332f70b85d0e1a94b3ee9e52b96cb
vefremov <efremov.vitaly@gmail.com>
2020-01-24 13:46:35 +0700

    Add solution to get most wanter letter

---
+++ home/most_wanted_letter.py
@@ -0,0 +1,22 @@
+import re
+from collections import Counter
+
+
+def checkio(text: str) -> str:
+    lower_text = text.lower()
+    chars = ''.join(re.findall(r'[a-z]+', lower_text))
+    char_statistics = Counter(chars)
+    best_char, _ = max(char_statistics.items(), key=lambda x: (x[1], -ord(x[0])))
+    
+    return best_char
+
+
+assert checkio("Hello World!") == "l", "Hello test"
+assert checkio("How do you do?") == "o", "O is most wanted"
+assert checkio("One") == "e", "All letter only once."
+assert checkio("Oops!") == "o", "Don't forget about lower case."
+assert checkio("AAaooo!!!!") == "a", "Only letters."
+assert checkio("abe") == "a", "The First."
+print("Start the long test")
+assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+print("The local tests are done.")
